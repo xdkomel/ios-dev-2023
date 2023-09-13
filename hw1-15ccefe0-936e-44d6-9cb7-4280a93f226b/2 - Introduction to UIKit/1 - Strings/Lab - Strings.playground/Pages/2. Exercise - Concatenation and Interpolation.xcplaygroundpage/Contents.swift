8c13b1ca853fff38f05406694e33aac37b4f5901
xdkomel <xdghost.kamil@gmail.com>
2023-09-13 22:34:45 +0300

    strings done

--- hw1-15ccefe0-936e-44d6-9cb7-4280a93f226b/2 - Introduction to UIKit/1 - Strings/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
+++ hw1-15ccefe0-936e-44d6-9cb7-4280a93f226b/2 - Introduction to UIKit/1 - Strings/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
@@ -3,12 +3,17 @@
  
  Create a `city` constant and assign it a string literal representing your home city. Then create a `state` constant and assign it a string literal representing your home state. Finally, create a `home` constant and use string concatenation to assign it a string representing your home city and state (i.e. Portland, Oregon). Print the value of `home`.
  */
+let city = "Kazan"
 
+let state = "Tatarstan"
 
+let home = "\(state), \(city)"
 //:  Use the compound assignment operator (`+=`) to add `home` to `introduction` below. Print the value of `introduction`.
 var introduction = "I live in "
 
+introduction += home
 
+print(introduction)
 /*:
  Declare a `name` constant and assign it your name as a string literal. Then declare an `age` constant and give it your current age as an `Int`. Then print the following phrase using string interpolation:
  
@@ -16,8 +21,11 @@ var introduction = "I live in "
  
  Insert `name` where indicated, and insert a mathematical expression that evaluates to your current age plus one where indicated.
  */
+let name = "Kamil"
 
+let age = 21
 
+print("My name is \(name) and after my next birthday I will be \(age + 1) years old")
 /*:
 [Previous](@previous)  |  page 2 of 5  |  [Next: App Exercise - Notifications](@next)
  */
