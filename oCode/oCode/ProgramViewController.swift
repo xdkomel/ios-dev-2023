3c6c7b0beb558286d24be759d889f0cb882c0478
xdkomel <xdghost.kamil@gmail.com>
2023-11-12 19:07:30 +0300

    added core data storage

--- oCode/oCode/ProgramViewController.swift
+++ oCode/oCode/ProgramViewController.swift
@@ -18,33 +18,37 @@ class ProgramViewController: UIViewController {
     private let compilerApi = MoyaProvider<Compiler>()
     private var subscriptions = Set<AnyCancellable>()
     private var viewModel: ProgramViewModel
-    // Program model
-    private let programCode: String
-    private let programInput: String?
-    private let programName: String
-    private let programTarget: TargetLanguage
+    // Persistence
+    private let storage: Storage
+    private let program: ProgramDataModel
     
-    init(
-        programCode: String,
-        programInput: String?,
-        programName: String,
-        programTarget: TargetLanguage
-    ) {
-        self.programCode = programCode
-        self.programInput = programInput
-        self.programName = programName
-        self.programTarget = programTarget
+    init(program: ProgramDataModel, storage: Storage) {
         viewModel = .init(
-            name: programName,
-            code: programCode,
-            target: programTarget,
-            input: programInput
+            name: program.name ?? "Program \(Int.random(in: 1...1000))",
+            code: program.code ?? "print(int(input()))",
+            target: .init(
+                compilerName: program.language?.tag ?? "py",
+                fullName: program.language?.fullName ?? "Python"
+            ),
+            input: program.input
         )
+        self.program = program
+        self.storage = storage
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        program.name = viewModel.name
+        program.code = viewModel.code
+        program.language?.fullName = viewModel.target.fullName
+        program.language?.tag = viewModel.target.compilerName
+        program.input = viewModel.input
+        storage.save()
+        super.viewWillDisappear(animated)
     }
     
     override func viewDidLoad() {
@@ -53,7 +57,7 @@ class ProgramViewController: UIViewController {
         view.backgroundColor = .systemBackground
         self.navigationItem.titleView = {
             let title = UILabel()
-            title.text = programName
+            title.text = viewModel.name
             title.font = .boldSystemFont(ofSize: UIFont.buttonFontSize)
             return title
         }()
@@ -64,7 +68,7 @@ class ProgramViewController: UIViewController {
             action: #selector(openModal)
         )
         
-        codeText.text = programCode
+        codeText.text = viewModel.code
         codeText.isScrollEnabled = true
         codeText.isEditable = true
         codeText.isSelectable = true
@@ -84,7 +88,6 @@ class ProgramViewController: UIViewController {
     func setBindings() {
         // View -> (ViewModel + View)
         codeText.textPublisher
-            .prepend([programCode])
             .receive(on: RunLoop.main)
             .debounce(for: .seconds(1), scheduler: DispatchQueue.main)
             .sink { [weak self] text in
@@ -116,13 +119,13 @@ class ProgramViewController: UIViewController {
     @objc func openModal() {
         self.navigationController?.present(
             {
-                let vc = UINavigationController(
+                let modal = UINavigationController(
                     rootViewController: ProgramRunViewController(
                         viewModel: viewModel
                     )
                 )
-                vc.modalPresentationStyle = .pageSheet
-                return vc
+                modal.modalPresentationStyle = .pageSheet
+                return modal
             }(),
             animated: true
         )
