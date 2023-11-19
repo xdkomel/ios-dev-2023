91c8026a28b092a0908110b0312175277f2be615
xdkomel <xdghost.kamil@gmail.com>
2023-11-20 01:05:03 +0300

    warnings fixes

--- oCode/oCodeTests/oCodeTests.swift
+++ oCode/oCodeTests/oCodeTests.swift
@@ -9,7 +9,6 @@ import XCTest
 @testable import oCode
 
 final class OCodeTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -32,5 +31,4 @@ final class OCodeTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
